7a348a74e2efbe960b7fc78c093427ce469026f7
Marshall <marshallzafiriou427@gmail.com>
2026-08-01 13:31:00 -0400

    bug fix

--- board.py
+++ board.py
@@ -1,19 +1,19 @@
  # Valid Directions
-        ROOK_DIRECTIONS = [
-            (1, 0),    # up
-            (-1, 0),   # down
-            (0, 1),    # right
-            (0, -1)    # left
-        ]
+ROOK_DIRECTIONS = [
+    (1, 0),    # up
+    (-1, 0),   # down
+    (0, 1),    # right
+    (0, -1)    # left
+]
 
-        BISHOP_DIRECTIONS = [
-            (1, 1),    # up-right
-            (1, -1),   # up-left
-            (-1, 1),   # down-right
-            (-1, -1)   # down-left
-        ]
+BISHOP_DIRECTIONS = [
+    (1, 1),    # up-right
+    (1, -1),   # up-left
+    (-1, 1),   # down-right
+    (-1, -1)   # down-left
+]
 
-        QUEEN_DIRECTIONS = ROOK_DIRECTIONS + BISHOP_DIRECTIONS
+QUEEN_DIRECTIONS = ROOK_DIRECTIONS + BISHOP_DIRECTIONS
 
 class Board:
     def __init__(self):
